0dd45a56d9e05a8decbd7d366c8de9a5c3e09f16
kazuyayagyu <kazuya.yagyu@cct-inc.co.jp>
2024-12-13 22:01:27 +0900

    Wiki

---
+++ docs/Wiki/index.md
@@ -0,0 +1,4 @@
+# はじめに
+
+Docusaurusの導入方法とTipsについてWikiに記載します。\
+実際の設定例については[Showcase](/docs/Sample)を確認してみてください。
--- docs/Wiki/sidebar.ts
+++ docs/Wiki/sidebar.ts
@@ -11,7 +11,79 @@ import type { SidebarsConfig } from "@docusaurus/plugin-content-docs";
  Create as many sidebars as you want.
  */
 const sidebar: SidebarsConfig = {
-  wikiSidebar: ["Wiki/index"],
+  wikiSidebar: [
+    {
+      type: "doc",
+      id: "Wiki/index",
+      label: "はじめに",
+    },
+    {
+      type: "category",
+      label: "Basic",
+      collapsed: false,
+      items: [
+        {
+          type: "doc",
+          id: "Wiki/Basic/introduction",
+          label: "Docusaurusとは",
+        },
+        {
+          type: "doc",
+          id: "Wiki/Basic/guide",
+          label: "Docusaurus基本設定",
+        },
+        {
+          type: "doc",
+          id: "Wiki/Basic/plugin",
+          label: "Docusaurusプラグイン設定",
+        },
+      ],
+    },
+    {
+      type: "category",
+      label: "Advanced",
+      collapsed: false,
+      items: [
+        {
+          type: "doc",
+          id: "Wiki/Advanced/googleDrive",
+          label: "GoogleDriveファイルの埋め込み",
+        },
+        {
+          type: "doc",
+          id: "Wiki/Advanced/figma",
+          label: "Figmaデザインの埋め込み",
+        },
+        {
+          type: "doc",
+          id: "Wiki/Advanced/lint",
+          label: "Lint設定",
+        },
+        {
+          type: "doc",
+          id: "Wiki/Advanced/openapi",
+          label: "OpenApiファイルの自動生成",
+        },
+      ],
+    },
+    {
+      type: "category",
+      label: "Tips",
+      collapsed: false,
+      items: [
+        {
+          type: "doc",
+          id: "Wiki/Others/document",
+          label: "Documentの書き方",
+        },
+        {
+          type: "doc",
+          id: "Wiki/Others/markdown",
+          label: "Markdownの記述方法",
+        },
+      ],
+    },
+  ],
 };
 
 export default sidebar.wikiSidebar;
